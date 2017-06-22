48fdc53bc76ef2e607eaa326541fbef2925fa2ab
quan <tranmqt@gmail.com>
2017-06-22 15:02:23 -0700

    set standard js style

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-// Force HTTPS connections
-// const enforce = require('express-sslify');
-//app.use(enforce.HTTPS({trustProtoHeader: true}));
+app.set('path', process.env.PORT || 8080)
 
 // serve static assets normally
 app.use(express.static(__dirname))
 
 app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'index.html'))
-});
+  response.sendFile(path.resolve(__dirname, 'index.html'))
+})
 
-app.listen(process.env.PORT || 8080);
+app.listen(app.get('path'))
+console.log('Test server listening on port ' + app.get('path'))
